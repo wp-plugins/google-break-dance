519262bba602990d327371f51b0ac169e379bf6c
ewwink <ewwink@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-05 08:06:38 +0000

    first
    git-svn-id: https://plugins.svn.wordpress.org/google-break-dance/trunk@663617 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ google-break-dance.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: Google Break Dance
+Plugin URI: http://*
+Description: Redirect Visitor dari Google image ke langsung halaman Post, jangan lupa edit file <b>.htaccess</b> sebelum menggunakan Plugin ini. Oh iya klo berguna, kapan2 jgn lupa cendolnya buat <strong><a href="http://www.cekpr.com">cekPR.com</a></strong> heheh...
+Version: 0.1
+Author: ewwink
+Author URI: http://www.cekpr.com/
+License: GPL2
+*/
+
+function cekpr_image_id($image_url) {
+    global $wpdb;
+    $prefix = $wpdb->prefix;
+    $attachment = $wpdb->get_col($wpdb->prepare("SELECT ID FROM " . $prefix . "posts" . " WHERE guid='" . $image_url . "';"));
+    return $attachment[0];
+}
+function Google_Image_Dance(){
+ if(preg_match('/get_image?.*\.(jpe?g|gif|png|ico)$/i',$_SERVER['REQUEST_URI'])){
+  $getImg = str_ireplace('get_image?','',$_SERVER['REQUEST_URI']);
+  if(cekpr_image_id(get_site_url().$getImg)){
+    $GattachId = get_permalink(cekpr_image_id(get_site_url().$getImg));
+    $GpId = cekpr_image_id(get_site_url().$getImg);
+    $GaUrl = get_post($GpId);
+    $GPostId = get_permalink($GaUrl->post_parent);
+    if($GPostId){
+      header("HTTP/1.1 301 Moved Permanently");
+      header("Location: $GPostId");
+      exit;
+    }
+    else{
+      header("HTTP/1.1 301 Moved Permanently");
+      header("Location: $GattachId");
+      exit;
+    }
+  }
+}
+}
+add_action('init','Google_Image_Dance',1);
+
+
+?>
