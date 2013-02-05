80bb99dce59d620371810e8a82c3927b1ec7d47a
ewwink <ewwink@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-05 14:13:11 +0000

    update $wbdb
    git-svn-id: https://plugins.svn.wordpress.org/google-break-dance/trunk@663740 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- google-break-dance.php
+++ google-break-dance.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Google Break Dance
-Plugin URI: http://*
+Plugin URI: http://wordpress.org/extend/plugins/google-break-dance/
 Description: Redirect Visitor dari Google image ke langsung halaman Post, jangan lupa edit file <b>.htaccess</b> sebelum menggunakan Plugin ini. Oh iya klo berguna, kapan2 jgn lupa cendolnya buat <strong><a href="http://www.cekpr.com">cekPR.com</a></strong> heheh...
-Version: 0.1
+Version: 0.2
 Author: ewwink
 Author URI: http://www.cekpr.com/
 License: GPL2
@@ -12,7 +12,8 @@ License: GPL2
 function cekpr_image_id($image_url) {
     global $wpdb;
     $prefix = $wpdb->prefix;
-    $attachment = $wpdb->get_col($wpdb->prepare("SELECT ID FROM " . $prefix . "posts" . " WHERE guid='" . $image_url . "';"));
+    $attachment = $wpdb->get_col($wpdb->prepare("SELECT ID FROM " . $prefix . "posts WHERE guid=%s;", $image_url));
+    //$attachment = $wpdb->get_col($wpdb->prepare("SELECT ID FROM " . $prefix . "posts" . " WHERE guid='" . $image_url . "';"));
     return $attachment[0];
 }
 function Google_Image_Dance(){
@@ -20,6 +21,7 @@ function Google_Image_Dance(){
   $getImg = str_ireplace('get_image?','',$_SERVER['REQUEST_URI']);
   if(cekpr_image_id(get_site_url().$getImg)){
     $GattachId = get_permalink(cekpr_image_id(get_site_url().$getImg));
+    $GoriUrl = get_site_url().$getImg;
     $GpId = cekpr_image_id(get_site_url().$getImg);
     $GaUrl = get_post($GpId);
     $GPostId = get_permalink($GaUrl->post_parent);
@@ -29,9 +31,16 @@ function Google_Image_Dance(){
       exit;
     }
     else{
-      header("HTTP/1.1 301 Moved Permanently");
-      header("Location: $GattachId");
-      exit;
+      if($GattachId){
+        header("HTTP/1.1 301 Moved Permanently");
+        header("Location: $GattachId");
+        exit;
+      }
+      else{
+        header("HTTP/1.1 301 Moved Permanently");
+        header("Location: $GoriUrl");
+        exit;
+      }
     }
   }
 }
